bccabfb47b3e38313678c3532da7444d3a42180b
Tim Davis <tim@pulsem.me>
2020-12-04 00:23:35 -0600

    Run pycov on git diff files with origin/master

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ TODO:
   3) Git hook that won't allow non python 2/3 code to be pushed.
 """
 import os
+import subprocess
 import sys
 
 from futurizer import futurize_code
 from mgmt.const import KILI_ROOT
+
+
+def get_diff_python_files(root, compare_branch='origin/master'):
+    """
+    :type root: str
+    :type compare_branch: str
+    :rtype: list of str
+    """
+    output = subprocess.check_output(['git', 'diff', '--name-only', compare_branch], cwd=root)
+    files = output.strip().split('\n')
+    return [os.path.join(root, f) for f in files if f.endswith('.py')]
 
 
 def get_all_python_files(root):
@@ -20,7 +32,7 @@ def get_all_python_files(root):
     """
     python_files = [os.path.join(root, 'kilimanjaro.py')]
     # for sub_directory in ['kilimanjaro_src', 'testing', 'assets']:
-    for sub_directory in ['kilimanjaro_src/comm/']:
+    for sub_directory in []:
         for subdir, _, files in os.walk(os.path.join(root, sub_directory)):
             for file_name in files:
                 if file_name.endswith('.py'):
@@ -29,7 +41,8 @@ def get_all_python_files(root):
 
 
 def python3_lint():
-    python_files = get_all_python_files(KILI_ROOT)
+    # python_files = get_all_python_files(KILI_ROOT)
+    python_files = get_diff_python_files(KILI_ROOT)
     # Rules I probably want: fix_xrange
     # Rules I want to avoid: list(Dict.items())
     # `dict`: We want to avoid using this initially because it makes us do things like list(keys()), but without it
